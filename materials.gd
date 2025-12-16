289be9dfe9f44b16efa18be3f99f363aed0c171e
natrapdev <natrap2007@gmail.com>
2025-12-16 16:35:28 -0500

    Added absorption value to materials

--- materials.gd
+++ materials.gd
@@ -2,20 +2,24 @@ extends Node
 
 var material_data: Dictionary = {
 	# density is measured in grams per cubic centimetre (g/cm^3)
-	"skin": {
-		"density": 1.1
+	"flesh": {
+		"density": 0.985,
+		"absorption": 0
 	},
 	"gambeson": {
 		"density": 1.3, # Per layer - total should be calculated density * numberOfLayers
+		"absorption": 0.7
 	},
 	"chainmail": {
 		"density": 4,
+		"absorption": 0.15
 	},
 	"iron": {
-		"density": 7.874
+		"density": 7.874,
+		"absorption": 0
 	},
 	"steel": {
-		"density": 7.85
+		"density": 7.85,
+		"absorption": 0
 	}
-	
 }
